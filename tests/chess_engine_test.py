951694a18e103ad99f470b5d3cb5443cbf50809b
PraxTube <lukinator28@gmail.com>
2023-05-21 09:17:11 +0200

    Add tests for promotion and castling

--- tests/chess_engine_test.py
+++ tests/chess_engine_test.py
@@ -74,6 +74,71 @@ def test_get_valid_moves():
         assert len(expected_moves) == len(actual_moves)
         assert expected_moves == actual_moves
 
+    board = chess.GameState("8/Ppk5/2p5/2P5/2KP4/8/8/8 w - - 0 1")
+    expected_moves = ["a8Q", "Kb4", "Kb3", "Kc3", "Kd3", "d5"]
+    promotion_move = None
+
+    for i, move in enumerate(board.getValidMoves()):
+        assert expected_moves[i] == str(move)
+        if str(move) == "a8Q":
+            promotion_move = move
+
+    board.makeMove(promotion_move)
+    assert "Q7/1pk5/2p5/2P5/2KP4/8/8/8 b - - 0 1" == board.fen()
+
+    board = chess.GameState(
+        "r1bqk2r/pppp1ppp/2n2n2/4p3/1bB1P3/2N2N2/PPPP1PPP/R1BQK2R w KQkq - 0 1"
+    )
+    expected_moves = [
+        "Bb5",
+        "Ba6",
+        "Bd5",
+        "Be6",
+        "Bxf7",
+        "Bd3",
+        "Be2",
+        "Bf1",
+        "Bb3",
+        "Nb5",
+        "Nd5",
+        "Ne2",
+        "Nb1",
+        "Na4",
+        "Nxe5",
+        "Ng5",
+        "Nh4",
+        "Ng1",
+        "Nd4",
+        "a3",
+        "a4",
+        "b3",
+        "d3",
+        "d4",
+        "g3",
+        "g4",
+        "h3",
+        "h4",
+        "Rb1",
+        "Qe2",
+        "Ke2",
+        "Kf1",
+        "Rg1",
+        "Rf1",
+        "0-0",
+    ]
+    promotion_move = None
+
+    for i, move in enumerate(board.getValidMoves()):
+        assert expected_moves[i] == str(move)
+        if str(move) == "0-0":
+            promotion_move = move
+
+    board.makeMove(promotion_move)
+    assert (
+        "r1bqk2r/pppp1ppp/2n2n2/4p3/1bB1P3/2N2N2/PPPP1PPP/R1BQ1RK1 b - - 0 1"
+        == board.fen()
+    )
+
 
 def test_get_all_possible_moves():
     board = chess.GameState()
